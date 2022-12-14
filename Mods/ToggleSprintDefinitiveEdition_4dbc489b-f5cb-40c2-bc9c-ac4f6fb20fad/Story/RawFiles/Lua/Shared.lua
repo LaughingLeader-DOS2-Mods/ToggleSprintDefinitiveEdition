31d5b6fe7de3ac0b5d9653038aab6dfea35e83a2
LaughingLeader <laughingleader@gmail.com>
2022-12-13 22:42:39 -0600

    Update API usage

--- Mods/ToggleSprintDefinitiveEdition_4dbc489b-f5cb-40c2-bc9c-ac4f6fb20fad/Story/RawFiles/Lua/Shared.lua
+++ Mods/ToggleSprintDefinitiveEdition_4dbc489b-f5cb-40c2-bc9c-ac4f6fb20fad/Story/RawFiles/Lua/Shared.lua
@@ -10,13 +10,14 @@ local function GetSpeedFlag(db, val)
 	if status then
 		return result
 	else
-		Ext.PrintError("[ToggleSprint] Error getting speed flag:")
-		Ext.PrintError(result)
+		Ext.Utils.PrintError("[ToggleSprint] Error getting speed flag:")
+		Ext.Utils.PrintError(result)
 	end
 	return nil
 end
 
-local function OnSettingsLoaded()
+---@param e LeaderLibSubscribableEventArgs|ModSettingsLoadedEventArgs
+local function OnSettingsLoaded(e)
 	-- Delete old settings
 	local oldDB = Osi.DB_LeaderLib_ModApi_GlobalSettings_Register_GlobalFlag:Get("3cdec0ff-5d98-e859-87cd-c663b7b9fcfe", nil, nil)
 	if oldDB ~= nil and #oldDB > 0 then
@@ -25,12 +26,14 @@ local function OnSettingsLoaded()
 	end
 
 	---@type ModSettings
-	local settings = Mods.LeaderLib.SettingsManager.GetMod(ModuleUUID, false)
+	local settings = e.Settings
 
-	local partySprint = math.min(11, math.max((settings.Global.Variables.PartySprintingSpeed.Value or 5), 2))
-	local enemySprint = math.min(11, math.max((settings.Global.Variables.EnemySprintingSpeed.Value or 5), 2))
-	local partyWalk = (settings.Global.Variables.PartyWalkingSpeed.Value or 2)
-	local enemyWalk = (settings.Global.Variables.EnemyWalkingSpeed.Value or 2)
+	local clamp = Mods.LeaderLib.GameHelpers.Math.Clamp
+
+	local partySprint = clamp(settings.Global:GetVariable("PartySprintingSpeed", 5), 4, 11)
+	local enemySprint = clamp(settings.Global:GetVariable("EnemySprintingSpeed", 5), 4, 11)
+	local partyWalk = clamp(settings.Global:GetVariable("PartyWalkingSpeed", 2), 2, 9)
+	local enemyWalk = clamp(settings.Global:GetVariable("EnemyWalkingSpeed", 2), 2, 9)
 
 	local flag = GetSpeedFlag("DB_ToggleSprint_SprintingSpeed", partySprint)
 	if flag ~= nil then
@@ -50,67 +53,57 @@ local function OnSettingsLoaded()
 		Osi.DB_ToggleSprint_EnemyWalkingSpeed:Delete(nil)
 		Osi.DB_ToggleSprint_EnemyWalkingSpeed(flag)
 	end
-end
 
-Ext.RegisterListener("SessionLoaded", function()
-	if Mods.LeaderLib ~= nil then
-		---@type ModSettings
-		local settings = Mods.LeaderLib.SettingsManager.GetMod(ModuleUUID, false)
-		if settings then
-			---@param self SettingsData
-			---@param name string
-			---@param data VariableData
-			-- settings.OnVariableSet = function(self, uuid, name, data)
-
-			-- end
-		
-			---@param self SettingsData
-			---@param name string
-			---@param data VariableData
-			settings.UpdateVariable = function(self, name, data)
-				if string.find(name, "SprintingSpeed") then
-					local flag = "ToggleSprint_SprintingSpeed_02"
-					if name == "PartySprintingSpeed" then
-						local flagEntry = Osi.DB_ToggleSprint_PartySprintingSpeed:Get(nil)
-						if flagEntry ~= nil then
-							flag = flagEntry[1][1]
-						end
-					elseif name == "EnemySprintingSpeed" then
-						local flagEntry = Osi.DB_ToggleSprint_EnemySprintingSpeed:Get(nil)
-						if flagEntry ~= nil then
-							flag = flagEntry[1][1]
-						end
-					end
-					local speedEntry = Osi.DB_ToggleSprint_SprintingSpeed:Get(nil, flag, nil)
-					if speedEntry ~= nil then
-						data.Value = speedEntry[1][3]
-					end
-				elseif string.find(name, "WalkingSpeed") then
-					local flag = "ToggleSprint_WalkingSpeed_00"
-					if string.find(name, "Party") then
-						local flagEntry = Osi.DB_ToggleSprint_PartyWalkingSpeed:Get(nil)
-						if flagEntry ~= nil then
-							flag = flagEntry[1][1]
-						end
-					elseif string.find(name, "Enemy") then
-						local flagEntry = Osi.DB_ToggleSprint_EnemyWalkingSpeed:Get(nil)
-						if flagEntry ~= nil then
-							flag = flagEntry[1][1]
-						end
-					end
-					local speedEntry = Osi.DB_ToggleSprint_WalkingSpeed:Get(nil, flag, nil)
-					if speedEntry ~= nil then
-						data.Value = speedEntry[1][3]
-					end
+	---@param self SettingsData
+	---@param name string
+	---@param data VariableData
+	settings.UpdateVariable = function(self, name, data)
+		if string.find(name, "SprintingSpeed") then
+			local flag = "ToggleSprint_SprintingSpeed_02"
+			if name == "PartySprintingSpeed" then
+				local flagEntry = Osi.DB_ToggleSprint_PartySprintingSpeed:Get(nil)
+				if flagEntry ~= nil then
+					flag = flagEntry[1][1]
+				end
+			elseif name == "EnemySprintingSpeed" then
+				local flagEntry = Osi.DB_ToggleSprint_EnemySprintingSpeed:Get(nil)
+				if flagEntry ~= nil then
+					flag = flagEntry[1][1]
 				end
 			end
-			if Ext.IsServer() then
-				OnSettingsLoaded()
+			local speedEntry = Osi.DB_ToggleSprint_SprintingSpeed:Get(nil, flag, nil)
+			if speedEntry ~= nil then
+				data.Value = speedEntry[1][3]
+			end
+		elseif string.find(name, "WalkingSpeed") then
+			local flag = "ToggleSprint_WalkingSpeed_00"
+			if string.find(name, "Party") then
+				local flagEntry = Osi.DB_ToggleSprint_PartyWalkingSpeed:Get(nil)
+				if flagEntry ~= nil then
+					flag = flagEntry[1][1]
+				end
+			elseif string.find(name, "Enemy") then
+				local flagEntry = Osi.DB_ToggleSprint_EnemyWalkingSpeed:Get(nil)
+				if flagEntry ~= nil then
+					flag = flagEntry[1][1]
+				end
+			end
+			local speedEntry = Osi.DB_ToggleSprint_WalkingSpeed:Get(nil, flag, nil)
+			if speedEntry ~= nil then
+				data.Value = speedEntry[1][3]
 			end
 		end
-
-		if Ext.IsServer() then
-			Mods.LeaderLib.RegisterListener("ModSettingsLoaded", OnSettingsLoaded)
-		end
 	end
-end)
+end
+
+if Ext.IsServer() then
+	if Mods.LeaderLib ~= nil then
+		Mods.LeaderLib.Events.ModSettingsLoaded:Subscribe(OnSettingsLoaded, {MatchArgs={UUID=ModuleUUID}})
+	else
+		Ext.Events.SessionLoaded:Subscribe(function(e)
+			if Mods.LeaderLib ~= nil then
+				Mods.LeaderLib.Events.ModSettingsLoaded:Subscribe(OnSettingsLoaded, {MatchArgs={UUID=ModuleUUID}})
+			end
+		end)
+	end
+end
